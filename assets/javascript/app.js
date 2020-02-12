7c09f6616725acefa3f35c38e15476acfdca769c
Aja Strommen <9466667+astrommen@users.noreply.github.com>
2020-02-12 11:15:07 -0500

    got movie posters to show up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
-
+   
 
     var x = "italian";
     var spoonQuery = "https://api.spoonacular.com/recipes/search?query=" + 
-        x + "&number=10&apiKey=b998635bd1cd4b36826270ed3c112703";
+        x + "&number=10&apiKey=c1b444b47d7b47e3a9f138fef3e699cd";
 
-    var y = "romance";  
-    var ombdQuery = "http://img.omdbapi.com/?apikey=trilogy&t="  
+    var y = "10749";  //tmdb genre id for romance is 10749
+    var tmdbQuery = "https://api.themoviedb.org/3/discover/movie?api_key=0c26415454ad6b4927743c99caee27b5&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=10&with_genres="+ y;
 
     // API call
     $.ajax({
@@ -28,6 +28,33 @@ $(document).ready(function() {
             $(".recipes").append(pRecipe);
     
         });
+        
+    });
+    // API call
+    $.ajax({
+        url: tmdbQuery,
+        method: "GET",
+    })
+    .then(function(response) { console.log(response);
+       
+
+        var results = response.results;
+             
+        var movieDiv = $(".movies"); // assign var to div for modularity
+
+        var tmdbImgUrl = "https://image.tmdb.org/t/p/w220_and_h330_face/";
+
+        $.each(results, function(index) {
+            
+            // create new img tags for 
+            var movieImg = $("<img>");
+            
+            // adds src link to img tag
+            movieImg.attr("src", tmdbImgUrl + results[index].poster_path); 
+            
+            // appends img tag to movie div
+            movieDiv.append(movieImg);
+        })
         
     });
 
