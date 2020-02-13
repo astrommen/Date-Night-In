2c47eeaa3b6b4adb699b3814a17ac2d9e688edc4
Aja Strommen <9466667+astrommen@users.noreply.github.com>
2020-02-13 17:57:50 -0500

    got recipe images + title to show; used edamam api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,29 +3,41 @@ $(document).ready(function() {
    
 
     var x = "italian";
-    var spoonQuery = "https://api.spoonacular.com/recipes/search?query=" + 
-        x + "&number=10&apiKey=c1b444b47d7b47e3a9f138fef3e699cd";
+    var edamamQuery = "https://api.edamam.com/search?q=" + 
+        x + "&app_id=4063f31e&app_key=02c947260b3a28a9dace374d2233e77e&from=0&to=10";
 
+        //https://webknox.com/recipeImages/swiss-cheese-fondue-with-english-stilton-773508.jpeg
+        //https://api.spoonacular.com/recipes/search?query=fondue&number=10&apiKey=70f3c998a06f47cfbcc87a31f2ed8aae
+        
     var y = "10749";  //tmdb genre id for romance is 10749
-    var tmdbQuery = "https://api.themoviedb.org/3/discover/movie?api_key=0c26415454ad6b4927743c99caee27b5&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=10&with_genres="+ y;
+
+    var tmdbQuery = "https://api.themoviedb.org/3/discover/movie?api_key=0c26415454ad6b4927743c99caee27b5&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=5&with_genres="+ y; //found a way to make it random by page #
 
     // API call
     $.ajax({
-        url: spoonQuery,
+        url: edamamQuery,
         method: "GET",
     })
     .then(function(response) { console.log(response);
        
+        var results = response.hits;
 
-        var results = response.results;
+        var recipeDiv = $(".recipes");
 
         $.each(results, function(index){
-                
-            var pRecipe = $("<p>");
+            
+            var recipeTitle = $("<p>");
 
-            pRecipe.html(results[index].title);
+            recipeTitle.text(results[index].recipe.label);
 
-            $(".recipes").append(pRecipe);
+            var recipeImg = $("<img>");
+
+            recipeImg.attr("src", results[index].recipe.image);
+
+            recipeDiv.append(
+                recipeTitle,
+                recipeImg
+            );
     
         });
         
@@ -37,7 +49,6 @@ $(document).ready(function() {
     })
     .then(function(response) { console.log(response);
        
-
         var results = response.results;
              
         var movieDiv = $(".movies"); // assign var to div for modularity
